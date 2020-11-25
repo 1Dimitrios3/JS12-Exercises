f800fd73e45ab7bf8e107dc0368ce490669ec2a9
1Dimitrios3 <dimitrismavrokefalos@gmail.com>
2020-11-25 19:04:52 +0200

    event examples to selectors.js

--- 2/js/selectors.js
+++ 2/js/selectors.js
@@ -1,65 +1,114 @@
-$( document ).ready(function(){
-    // $("*").css({
-    //     "background-color": "black",
-    //     "color": "white"
-    // });
+$(document).ready(function() {
+  // $("*").css({
+  //     "background-color": "black",
+  //     "color": "white"
+  // });
 
-    //background white && black on button click
-    const $blackBackground = $("*");
+  //background white && black on button click
+  const $blackBackground = $("*");
 
-    $(".allBlack").on("click", function(){
-        $blackBackground.css({
-            "background-color": "black", 
-            "color": "white"
-        });
-        $(this).on("click", function(){
-            $blackBackground.css({
-                "background-color": "white",
-                "color": "black"
-            });
+  $(".allBlack").on("click", function() {
+    $blackBackground.css({
+      "background-color": "black",
+      color: "white"
+    });
+    $(this).on("click", function() {
+      $blackBackground.css({
+        "background-color": "white",
+        color: "black"
+      });
+    });
+  });
 
-        });
+  //selections
+  $("a[hreflang|='en']").css("border", "5px double green");
+  // $("a[href='thereissomeexample.html']").fadeOut("10");
 
-    })
+  // $("li[class$='B']").mouseover(function(){
+  //     $(this).hide(3000)
+  // });
 
-    //selections
-    $("a[hreflang|='en']").css("border", "5px double green");
-    // $("a[href='thereissomeexample.html']").fadeOut("10");
+  // ΦΘΟΝΟΣ
+  // $("ul li:nth-child(2)").mouseover(function(){
+  //     $(this).css({
+  //         "font-size": "2em",
+  //         "color": "aqua"
+  //     });
+  // });
 
+  //select letter O
+  // $("li*:contains('Α')").css("text-decoration", "underline");
 
-    // $("li[class$='B']").mouseover(function(){
-    //     $(this).hide(3000)
-    // });
+  //change attributes
+  $("div>a").attr("href", "https://johnresig.com");
+  $("#nameValueSelector>a:odd").attr("href", "https://brendaneich.com");
+  $("div a:nth-child(3)")
+    .text("Ryan Dahl")
+    .attr("href", "http://ryandahl.com");
 
-    // ΦΘΟΝΟΣ
-    // $("ul li:nth-child(2)").mouseover(function(){
-    //     $(this).css({
-    //         "font-size": "2em",
-    //         "color": "aqua"
-    //     });
-    // });
+  //button animate
+  $(".grow-big-sins").click(function() {
+    const $ul = $(".seven-sins");
+    $ul.css("list-style-type", "none");
+    $ul.animate(
+      {
+        width: "70%",
+        opacity: 0.5,
+        marginLeft: ".7em",
+        fontSize: "3em"
+      },
+      1500
+    );
+  });
 
-    //select letter O
-    // $("li*:contains('Α')").css("text-decoration", "underline");
+  $(".doubleClick").on("dblclick", function() {
+    $(".growBig").animate({ fontSize: "4em", opacity: 0.3 }, 3000);
+  });
 
-    //change attributes
-    $("div>a").attr("href","https://johnresig.com");
-    $("#nameValueSelector>a:odd").attr("href", "https://brendaneich.com");
-    $("div a:nth-child(3)").text("Ryan Dahl").attr("href", "http://ryandahl.com");
+  $("pre").on({
+    mouseenter: function() {
+      $(this).css("background-color", "lightGrey");
+    },
+    mouseleave: function() {
+      $(this).css("background-color", "white");
+    },
+    click: function() {
+      $(this).css("background-color", "lightBlue");
+    }
+  });
 
-    //button animate
-    $(".grow-big-sins").click(function(){
-        const $ul = $(".seven-sins");
-        $ul.css("list-style-type", "none");
-        $ul.animate({
-            width: "70%",
-            opacity: .5,
-            marginLeft: ".7em",
-            fontSize: "3em",
-        }, 1500 );
+  $("input").keypress(function(event) {
+    if (event.which == 13) {
+      $(".keyPressed").text("you pressed enter");
+    }
+  });
+  $("input").on("keypress", function(e) {
+    $(".keyPressed").text(e.type + " : " + e.which);
+  });
+
+  // Add a p to article
+  const $leaveDoc = $("article");
+
+  $leaveDoc
+    .append(
+      "<p>Before you leave pass over with your mouse to underline the text and upon leaving click me</p>"
+    )
+    .addClass("go")
+    .on("mouseenter", function() {
+      $(this).css("text-decoration", "underline");
     });
 
-    $(".doubleClick").on("dblclick", function(){
-        $(".growBig").animate({fontSize: "4em", opacity: .3}, 3000)
+  const $section = $("section");
+
+  $section
+    .append("<h3>Here comes Santa Claus. But he doesnt have any gifts!</h3>")
+    .addClass("sect")
+    .on("mouseenter", function() {
+      $(this).css("background-color", "pink");
     });
-})
+
+  // change css on mouse enter event
+  //   $("article").on("mouseenter", function() {
+  //     $(this).css("text-decoration", "underline");
+  //   });
+});
